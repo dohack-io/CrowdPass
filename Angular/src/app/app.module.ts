5ace1bd1a2f476cd1108b6786f3ea15448d284ae
dschwarze <denise.schwarze016@stud.fh-dortmund.de>
2019-09-28 17:37:14 +0200

    shoppingList init

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import {MatListModule} from '@angular/material/list';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatListModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
