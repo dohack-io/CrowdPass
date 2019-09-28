cc7abc24ade085736c537f30a43c620a57958059
Valentin <valentin.saks@gmail.com>
2019-09-28 16:03:46 +0200

    add Profile and User

---
+++ Spring/doughnotList/src/main/java/com/doughhack/doughnotList/model/Profile.java
@@ -0,0 +1,69 @@
+package com.doughhack.doughnotList.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Profile extends Entity{
+    private String firstName;
+    private String lastName;
+    private String profileName;
+    private List<Preference> blacklist;
+
+    public Profile(String firstName, String lastName, String profileName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.profileName = profileName;
+        ArrayList<Preference> blacklist = new ArrayList();
+    }
+
+
+    @Override
+    public String toString() {
+        return "Profile{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", profileName='" + profileName + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Profile profile = (Profile) o;
+        return firstName.equals(profile.firstName) &&
+                lastName.equals(profile.lastName) &&
+                profileName.equals(profile.profileName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, profileName);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getProfileName() {
+        return profileName;
+    }
+
+    public void setProfileName(String profileName) {
+        this.profileName = profileName;
+    }
+}
+
---
+++ Spring/doughnotList/src/main/java/com/doughhack/doughnotList/model/User.java
@@ -0,0 +1,56 @@
+package com.doughhack.doughnotList.model;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class User extends Entity {
+    private String email;
+    private String hashedPW;
+    private Profile profile;
+
+    public User(String mail, String pw, String name, String vorname, String profilename) {
+        this.email = mail;
+        this.hashedPW = pw;
+
+        this.profile = new Profile(vorname, name, profilename);
+
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return email.equals(user.email);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "email='" + email + '\'' +
+                '}';
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(email);
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getHashedPW() {
+        return hashedPW;
+    }
+
+    public void setHashedPW(String hashedPW) {
+        this.hashedPW = hashedPW;
+    }
+
+}
