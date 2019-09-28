2ea57242e6f73dea5966f6f52a2427203ffb887b
ikornilow <iwan.kornilow001@stud.fh-dortmund.de>
2019-09-28 11:48:03 +0200

    init for spring

---
+++ Spring/doughnotList/src/main/java/com/doughhack/doughnotList/DoughnotListApplication.java
@@ -0,0 +1,13 @@
+package com.doughhack.doughnotList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoughnotListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoughnotListApplication.class, args);
+	}
+
+}
